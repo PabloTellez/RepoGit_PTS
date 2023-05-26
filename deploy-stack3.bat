12a34a5dfed2ccaaaab8ff2c5cf069e7be36869d
PabloTellez <74043227+PabloTellez@users.noreply.github.com>
2023-05-26 22:24:31 +0200

    Delete deploy-stack3.bat

--- deploy-stack3.bat
+++
@@ -1,8 +0,0 @@
-set Tinstancia=t2.small
-aws cloudformation deploy --stack-name WordPressServer --template-file main5.yml --parameter-override InstanceType=%Tinstancia%
-
-IF %errorlevel% neq 0 (
-	echo Hubo un error
-) else (
-	aws cloudformation list-exports --query "Exports[?Name == 'ServerPublicIp']"	
-)
